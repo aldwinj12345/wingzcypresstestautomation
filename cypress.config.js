137260f35f9b26a7defc50590fe74472306eb284
Aldwin J <aldwinj12345@gmail.com>
2025-02-20 23:38:20 +0800

    2nd commit

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,11 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://auth.wingz.me/auth/signin', //initial test url
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    screenshotOnRunFailure: true, // Capture screenshots on failure (default)
+    video: true, // Capture videos for all runs (default)
   },
 });
